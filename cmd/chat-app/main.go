6e6775ff15212efe29a435ca2c9cfdfa07d476d8
Gustavo B. <dsn4@wide.com.vc>
2024-06-18 14:20:53 -0300

    feat: add websocket logic for duplex communication

--- cmd/chat-app/main.go
+++ cmd/chat-app/main.go
@@ -1,23 +1,64 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"time"
+
+	"github.com/gorilla/websocket"
 )
 
-func main() {
-	fmt.Println("Listening on port 8000")
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8000", nil)
+type webSocketHandler struct {
+	upgrader websocket.Upgrader
+}
+type chatMessage struct {
+	Message string `json:"message"`
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func (wsh webSocketHandler) WSHandler(w http.ResponseWriter, r *http.Request) {
+	c, err := wsh.upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer c.Close()
+
+	mt, message, err := c.ReadMessage()
+	if err != nil {
+		log.Println(err)
+	}
+	if message == nil {
+		return
+	}
+	log.Printf("recv: %s\n at: %s", message, time.Now().Format(time.RFC1123))
+
+	var chatMessage chatMessage
+	json.Unmarshal([]byte(message), &chatMessage)
+
+	err = c.WriteMessage(mt, []byte(fmt.Sprintf(`<ul id="chat" hx-swap-oob="beforeend">%s</ul>`, chatMessage.Message)))
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
 		http.ServeFile(w, r, "web/static/chat.html")
-	case http.MethodPost:
-		fmt.Printf("got message: { %s } at { %s }\n", r.FormValue("message"), time.Now().Format(time.RFC1123))
-		w.Write([]byte("<h3>" + r.FormValue("message") + "</h3>"))
 	}
+}
+
+func init() {
+	log.Println("Listening on port 8000...")
+}
+
+func main() {
+	wsh := webSocketHandler{upgrader: websocket.Upgrader{}}
+
+	http.HandleFunc("/chat", wsh.WSHandler)
+	http.HandleFunc("/", homeHandler)
+	http.ListenAndServe(":8000", nil)
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/adeptusgb/chat-app
 
 go 1.22.4
+
+require github.com/gorilla/websocket v1.5.3
